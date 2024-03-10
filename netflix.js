dadf77208f9ccdd399d8d90ff6dc57cad02f6cbb
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-10 22:52:29 +0530

    changed

--- netflix.js
+++ netflix.js
@@ -2,8 +2,4 @@
 // wiehpofa npwafi ae;oig
 //footer>
 //<div class="logos">
-//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
-//</div>
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a
