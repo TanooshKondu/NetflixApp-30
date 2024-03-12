423b8d8002a09eee0893a982e88399fd2fce4741
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-12 21:09:10 +0530

    updated

--- netflix.js
+++ netflix.js
@@ -1,10 +1,4 @@
-//   <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/spider-man-far-from-home-international-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/venom-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/snake-eyes-gi-joe-origins-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/the-addams-family-2-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/luca-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/the-amazing-spider-man-movie-poster.png" alt="" /></a>
-//</div>
+
 //</section>
 //<!-- FOOTER -->
 //<footer>
