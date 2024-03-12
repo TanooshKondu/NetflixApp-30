3297aef32660f4a8aeffc8aa55da9ffa8dce1916
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-12 21:08:05 +0530

    updated

--- netflix.js
+++ netflix.js
@@ -1,13 +1,4 @@
-//   <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/spider-man-far-from-home-international-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/venom-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/snake-eyes-gi-joe-origins-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/the-addams-family-2-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/luca-movie-poster.png" alt="" /></a>
-//    <a href=""><img src="https://assets.ccbp.in/frontend/react-js/movies-app/the-amazing-spider-man-movie-poster.png" alt="" /></a>
-//</div>
-//</section>
-//<!-- FOOTER -->
-//<footer>
+
 //<div class="logos">
 //    <a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
 //    <a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
