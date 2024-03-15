a8b9d4d77e69202edc2d3d02da242707f82d7d2d
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-15 13:27:38 +0530

    updated

--- netflix.js
+++ netflix.js
@@ -17,9 +17,3 @@
 //</div>
 //<p>Contact US</p>
 //<p>Help center</p>
-//</footer>
-// <footer>
-//<div class="logos">
-//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
