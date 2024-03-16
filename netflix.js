b42fec73d34763606dc20b07277ef72500af010b
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-03-16 21:38:49 +0530

    updated

--- netflix.js
+++ netflix.js
@@ -9,27 +9,3 @@
 //<div class="logos">
 //<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
 //<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
-//</div>
-//ter>
-//<div class="logos">
-//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
-//</div>
-//ter>
-//<div class="logos">
-//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
-//</div>
-//ter>
-//<div class="logos">
-//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
-//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
-//</div>
