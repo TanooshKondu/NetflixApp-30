3d899c2b0fdc0d1914915d25b3900d08b7c8f599
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-04-08 22:17:38 +0530

    updated

--- netflix.js
+++ netflix.js
@@ -8,3 +8,418 @@
 //<p>Contact US</p>
 //<p>Help center</p>
 //</footer>
+//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>//<footer>
+//<div class="logos">
+//<a href="#"><i class="fab fa-facebook-square fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-instagram fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-twitter fa-2x logo"></i></a>
+//<a href="#"><i class="fab fa-youtube fa-2x logo"></i></a>
+//</div>
+//<p>Contact US</p>
+//<p>Help center</p>
+//</footer>
